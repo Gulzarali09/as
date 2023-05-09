e9b1d0fb941ae4a6e0f998bb5b54bd6e7068ff29
Gulzarali09 <121237439+Gulzarali09@users.noreply.github.com>
2023-05-09 13:13:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+function back()
+{
+    window.location = "index.html";
+}
+function next()
+{
+    window.location = "music2.html";
+}
+function up()
+{
+    window.location = "music1.html";
+}
+function down()
+{
+    window.location = "music3.html";
+}
+function n()
+{
+    window.location = "music2.html";
+}
+function r()
+{
+    window.location = "music4.html";
+}
+function c()
+{
+    window.location = "music3.html";
+}
+function a()
+{
+    window.location = "index.html";
+}
+
+function f()
+{
+    window.location = "index.html";
+}
